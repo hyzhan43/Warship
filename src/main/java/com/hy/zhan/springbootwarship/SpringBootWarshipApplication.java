06a9ea3424cded5d6787d53e17eff669fc4e3811
HyJame <1063523767@qq.com>
2019-11-25 22:36:42 +0800

    init project

---
+++ src/main/java/com/hy/zhan/springbootwarship/SpringBootWarshipApplication.java
@@ -0,0 +1,13 @@
+package com.hy.zhan.springbootwarship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWarshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWarshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hy/zhan/springbootwarship/SpringBootWarshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.zhan.springbootwarship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWarshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
